607cb5fe9e0f9f7c9d49762b9f90021418dc487f
ai-pc <ai-pc@emailadd.com>
2025-10-06 20:35:41 +0700

    update use real link

--- src/App.tsx
+++ src/App.tsx
@@ -196,8 +196,8 @@ function App() {
       formData.append('assigneeEmail', assigneeEmail || '');
       formData.append('difficulty', difficulty === '' ? '' : String(difficulty));
       /// load from .env
-      // const url = import.meta.env.VITE_API_BASE_URL + 'playable-ads-submit';
-      const url = "http://localhost:3888/api/playable-ads-submit";
+      const url = import.meta.env.VITE_API_BASE_URL + 'playable-ads-submit';
+      // const url = "http://localhost:3888/api/playable-ads-submit";
       const response = await fetch(url, {
         method: 'POST',
         body: formData,
